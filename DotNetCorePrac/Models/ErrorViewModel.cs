96e25f01acb0a86b3421f212dcadd3689de32df7
santu97 <santhoshbhattaram@gmail.com>
2023-08-10 11:13:08 -0500

    Add project files.

---
+++ DotNetCorePrac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCorePrac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
