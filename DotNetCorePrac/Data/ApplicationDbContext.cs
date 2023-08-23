218a333d59862880d4b5d0da000b08e227dd88b5
santu97 <santhoshbhattaram@gmail.com>
2023-08-23 15:04:43 -0500

    Adding DBContexts and Configurations

---
+++ DotNetCorePrac/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCorePrac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCorePrac.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
