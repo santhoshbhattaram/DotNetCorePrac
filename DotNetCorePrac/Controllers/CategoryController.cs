d361f2dda012e2bb4110cf14c36a5571b2fc9c24
santu97 <santhoshbhattaram@gmail.com>
2023-08-25 14:47:44 -0500

    Validations, HTML Helpers,

--- DotNetCorePrac/Controllers/CategoryController.cs
+++ DotNetCorePrac/Controllers/CategoryController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCorePrac.Data;
+using DotNetCorePrac.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCorePrac.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext) {  _dbContext = dbContext; }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _dbContext.Categories.ToList<Category>();
+            return View(categories);
         }
+            
+        public IActionResult Create() {
+
+            return View("AddNewCategory");
+        }
+        [HttpPost]
+        public IActionResult AddNewCategory(Category newCategory) {
+
+            // add custom error message from controller
+            if (newCategory.DisplayOrder == 50)
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order cannot be 50");
+            }
+
+
+            if (newCategory != null && ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(newCategory);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(); 
+        }
+
+
     }
 }
--- DotNetCorePrac/Models/Category.cs
+++ DotNetCorePrac/Models/Category.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
+/*
+ * what to learn in this page??
+Validations, Model Creation, Database schema (key), required for not null and html helpers for display orders 
+
+ */
 
 namespace DotNetCorePrac.Models
 {
@@ -10,8 +17,13 @@ namespace DotNetCorePrac.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Category Name should be mandatory")]
+        [DisplayName("Category Name")]
+        [MaxLength(30,ErrorMessage ="Maximum Length of Category Name should be 30")]
         public String  Name{ get; set; }
+        //for displaying in html by using html helper(html for)
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order should be between 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
